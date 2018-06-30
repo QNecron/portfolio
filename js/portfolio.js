1ba31efc379aa8e6d559b024e8b8affca43af546
Unknown <q.necron@gmail.com>
2018-06-29 23:05:52 -0500

    Getting back into this!
    JS flushed out for menu and footer. Updates to buttons.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,107 +1,139 @@
-//
-var pageContainer = document.querySelector('[data-page-container]');
-var menu = document.querySelector('[data-menu]');
-var menuTrigger = document.querySelector('[data-menu-trigger]');
-var dialog = document.querySelectorAll('[data-dialog]');
-var dialogTrigger = document.querySelectorAll('[data-dialog-trigger]');
-// var dialogOverlay = document.querySelector('[data-dialog-overlay]');
+/*
 
-//
-function ariaHidden(ele, attr) {
-  ele.setAttribute('aria-hidden', attr.toString());
-}
+  Portfolio JS
 
-//
-function menuSetup(link, menu) {
+*/
 
-  link.addEventListener('click', function(m) {
-    m.preventDefault();
-    menu.classList.toggle('open');
-    link.classList.toggle('open');
+document.addEventListener("DOMContentLoaded", function(event) {
 
-    // mobile
-    if(window.innerWidth <= 767) {
+  //
+  var pageContainer = document.querySelector('[data-page-container]');
+  var menu = document.querySelector('[data-menu]');
+  var menuTrigger = document.querySelector('[data-menu-trigger]');
+  var dialog = document.querySelectorAll('[data-dialog]');
+  var dialogTrigger = document.querySelectorAll('[data-dialog-trigger]');
+  // var dialogOverlay = document.querySelector('[data-dialog-overlay]');
+  var date = new Date();
+  var year = date.getFullYear();
+  var yearUpdate = document.querySelector('[data-year]');
 
-      if(link.getAttribute('aria-expanded') == 'false') {
-        link.setAttribute('aria-expanded', 'true');
+  //
+  function ariaHidden(ele, attr) {
+    ele.setAttribute('aria-hidden', attr.toString());
+  }
+
+  //
+  function menuSetup(link, menu) {
+
+    link.addEventListener('click', function(m) {
+      m.preventDefault();
+      menu.classList.toggle('open');
+      link.classList.toggle('open');
+
+      // mobile
+      if(window.innerWidth <= 767) {
+
+        if(link.getAttribute('aria-expanded') == 'false') {
+          link.setAttribute('aria-expanded', 'true');
+        }
+        else {
+          link.setAttribute('aria-expanded', 'false');
+        }
+
       }
-      else {
-        link.setAttribute('aria-expanded', 'false');
+
+    });
+
+  }
+
+  menuSetup(menuTrigger, menu);
+
+  //
+  function openDialog(trigger) {
+    var openTrigger = trigger.getAttribute('data-dialog-trigger');
+
+    for( var i = 0; i < dialog.length; i++ ) {
+
+      if( dialog[i].getAttribute('data-dialog') == openTrigger ) {
+        ariaHidden(dialog[i], false);
       }
 
-    }
-
-  });
-
-}
-
-menuSetup(menuTrigger, menu);
-
-//
-function openDialog(trigger) {
-  var openTrigger = trigger.getAttribute('data-dialog-trigger');
-
-  for( var i = 0; i < dialog.length; i++ ) {
-
-    if( dialog[i].getAttribute('data-dialog') == openTrigger ) {
-      ariaHidden(dialog[i], false);
     }
 
   }
 
-}
+  function closeDialog(ele) {
+    var closeDialog;
 
-function closeDialog(ele) {
-  var closeDialog;
+    for( var i = 0; i < ele.length; i++ ) {
 
-  for( var i = 0; i < ele.length; i++ ) {
+      closeDialog = ele[i].querySelector('[data-dialog-close]');
 
-    closeDialog = ele[i].querySelector('[data-dialog-close]');
+      closeDialog.addEventListener('click', function() {
+        ariaHidden(this.parentElement, true);
+        ariaHidden(pageContainer, false);
+        // dialogOverlay.setAttribute('data-dialog-overlay', 'open');
+      });
 
-    closeDialog.addEventListener('click', function() {
-      ariaHidden(this.parentElement, true);
-      ariaHidden(pageContainer, false);
+    }
+
+  }
+
+  function closeAllDialog(ele1, ele2) {
+
+    for( var i = 0; i < ele1.length; i++ ) {
+      ariaHidden(ele1[i], true);
+    }
+
+    ariaHidden(ele2, false);
+    // dialogOverlay.setAttribute('data-dialog-overlay', 'open');
+
+  }
+
+  for( var i = 0; i < dialogTrigger.length; i++ ) {
+
+    dialogTrigger[i].addEventListener('click', function(l) {
+      openDialog(this);
+      closeDialog(dialog);
+      ariaHidden(pageContainer, true);
       // dialogOverlay.setAttribute('data-dialog-overlay', 'open');
     });
 
   }
 
-}
+  // dialogOverlay.addEventListener('click', function(o) {
+  //   closeAllDialog(dialog, pageContainer);
+  // });
 
-function closeAllDialog(ele1, ele2) {
+  document.onkeyup = function(event) {
+    event = event || window.event;
 
-  for( var i = 0; i < ele1.length; i++ ) {
-    ariaHidden(ele1[i], true);
+    var e = event.keyCode;
+
+    // ECS key
+    if( e == 27 ) {
+      closeAllDialog(dialog, pageContainer);
+    }
+
   }
 
-  ariaHidden(ele2, false);
-  // dialogOverlay.setAttribute('data-dialog-overlay', 'open');
+  //
+  yearUpdate.innerHTML = year;
 
-}
+  //
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
-for( var i = 0; i < dialogTrigger.length; i++ ) {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
 
-  dialogTrigger[i].addEventListener('click', function(l) {
-    openDialog(this);
-    closeDialog(dialog);
-    ariaHidden(pageContainer, true);
-    // dialogOverlay.setAttribute('data-dialog-overlay', 'open');
+      console.log(this.getAttribute('href'));
+
+      document.getElementById(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+
+    });
+
   });
 
-}
-
-// dialogOverlay.addEventListener('click', function(o) {
-//   closeAllDialog(dialog, pageContainer);
-// });
-
-document.onkeyup = function(event) {
-  event = event || window.event;
-
-  var e = event.keyCode;
-
-  // ECS key
-  if( e == 27 ) {
-    closeAllDialog(dialog, pageContainer);
-  }
-
-}
+});
